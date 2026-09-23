dd183e520eee65f007598c0c64f2a8691bc23173
justjude34-ship-it <justjude34@gmail.com>
2026-09-23 18:00:22 +1000

    Bump Daily Plate cache to v4 for photoreal hero

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "daily-plate-v3";
+const CACHE = "daily-plate-v4";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon.svg", "./hero.svg"];
 
 self.addEventListener("install", (event) => {
